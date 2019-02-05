586b2bc77171dc0ba187d8eb91bfaa369a8ffdba
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2019-02-05 19:06:07 +0300

    Introduce Kafka Consumer

---
+++ kafka-java/src/main/java/com/example/kafka/ConsumerDemo.java
@@ -0,0 +1,39 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        consumer.subscribe(Collections.singleton("my-topic"));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("Topic: " + record.topic()
+                        + "\n Partition: " + record.partition()
+                        + "\n Offset: " + record.offset()
+                        + "\n Timestamp: " + record.timestamp()
+                        + "\n Key: " + record.key()
+                        + "\n Value: " + record.value());
+            }
+        }
+    }
+}
